5aaf0a07fdc055fd1c33227d617d2df373a71c3d
Eric Whitcomb <eric.whitcomb@gmail.com>
2019-08-05 17:47:08 -0600

    Completed ex1, all tests pass
    I believe this is linear

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -9,9 +9,24 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    if length < 2:
+        return None
+
+    for i in range(length):
+        # calculate other value
+        other_value = limit - weights[i]
+
+        # retrieve index of other value
+        j = hash_table_retrieve(ht, other_value)
+
+        # check if j not null and j more than i
+        if j is not None and j > i:
+            return (j, i)
+        elif j is not None: # or check if j not null which means j is less than i
+            return (i, j)
+        else: # else insert in ht
+            hash_table_insert(ht, weights[i], i)
+
 
     return None
 
